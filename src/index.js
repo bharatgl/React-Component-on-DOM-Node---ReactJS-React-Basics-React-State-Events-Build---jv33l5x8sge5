96e781b1c09dc04e1e4aa9752822c5614126111d
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-08-07 22:44:31 +0530

    update project

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
-const root = ReactDOM.createRoot(<App />,document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+      <App />
+);
